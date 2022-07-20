9a2cfd5d1a9c7708e12f0d6d164f5a4209562dc1
Felipe Fernandes <fesousalocutor@gmail.com>
2022-07-20 02:04:21 -0300

    add btmills prettier

--- src/main.js
+++ src/main.js
@@ -1,2 +1,13 @@
-const numero = 2
-console.log(numero)
+const numero = 2;
+console.log(numero);
+
+if (
+  numasdfasdfsdfasdfsadfsadfasdfero == 2
+  && numeasdfasdfsdfasdfsadfsadfasdfro == 2
+  && numeasdfasdfsdfasdfsadfsadfasdfro == 2
+  && numasdfasdfsdfasdfsadfsadfasdfero == 2
+  && numasdfasdfsdfasdfsadfsadfasdfero == 2
+  && numasdfasdfsdfasdfsadfsadfasdfero == 2
+  && numasdfasdfsdfasdfsadfsadfasdfero == 2
+) {
+}
